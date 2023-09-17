b77a339736cdbb513d438ffa2432c7fd801bdc8c
NVHAI <hai.nguyen6@propzy.com>
2023-09-17 16:47:39 +0700

    27:30 Animating the Player with Scripting

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,7 +6,10 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var anim = get_node("AnimatedSprite2D")
 
+func _ready():
+	anim.play("Idle")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,13 +19,16 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.play("Run")
 	else:
+		anim.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
