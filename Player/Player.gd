89d88ba1ee7bcabe81055de1a0ab88b89f20b49b
NVHAI <hai.nguyen6@propzy.com>
2023-09-30 23:51:53 +0700

    29:55 Player Camera and Game Window Sizes

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,8 +8,8 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimatedSprite2D")
 
-func _ready():
-	anim.play("Idle")
+#func _ready():
+#	anim.play("Idle")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,16 +19,16 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
+		#anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		anim.play("Run")
+		#anim.play("Run")
 	else:
-		anim.play("Idle")
+		#anim.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
